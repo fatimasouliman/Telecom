9c5f6b4da1b86ec7d4b90a9a78ea67d8b8d5a264
NourAhmadAli <nourahmadalii29@gmail.com>
2023-11-03 13:40:04 +0200

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
-import Home from './Pages/Home'
+import Navbar from './Components/navbar/Navbar';
+
+
 
 function App() {
 
   return (
     <div className='app'>
-      <Home />
+      <Navbar/>
+     
+     
     </div>
   )
 }
