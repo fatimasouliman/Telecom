85e9ce34effdbd1e70b71c8823444f85f8a6f318
fatima souliman <fffaat.sss@gmail.com>
2023-11-07 19:59:12 +0200

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import Navbar from './Components/navbar/Navbar';
 import Card from './Components/card/Card';
-import Services from './Components/services/Servi
+import Services from './Components/services/Services';
 import { Carousel } from './Components/carousel/Carousel';
 import {slides} from "./data/carouselData.json"
+import Footer from './Components/Footer/Footer';
 
 
 
@@ -15,8 +16,10 @@ function App() {
   return (
     <div className='app'>
       <Navbar/>
+      <Carousel data={slides} />
       <Services />
-     <Carousel data={slides} />
+      <Footer />
+     
 
      
     </div>
--- src/Components/card/Card.jsx
+++ src/Components/card/Card.jsx
@@ -2,19 +2,22 @@ import React from 'react'
 import { NavLink } from 'react-bootstrap'
 import "./card.css"
 import arrow from '../../assets/img/arrow.svg'
-function Card({image , title , desc}) {
+function Card({ image, title, desc }) {
   return (
     <div className="card-m col-lg-3 "  >
       <div className='image-card '>
-            <img className='img-card' src={image}  />
+        <img className='img-card' src={image} />
       </div>
-      <h2 className='card-title'>{title}</h2>
-      <div className='content' >
-      <NavLink><img src={arrow} /></NavLink>
-        <p className='card-desc' >{desc}</p>
-        
+      <div className='right-side' >
+        <h2 className='card-title'>{title}</h2>
+        <div className='content' >
+          <NavLink><img src={arrow} /></NavLink>
+          <p className='card-desc' >{desc}</p>
+
+        </div>
       </div>
-      
+
+
     </div>
   )
 }
